5714084996865d03be82e7f7f62cd274b23ad08b
Pratik Dhumal <68268541+pratikd2124@users.noreply.github.com>
2021-02-01 21:30:05 +0530

    Update stack.c

--- stack.c
+++ stack.c
@@ -9,23 +9,23 @@ int pop();
 int peek();
 void display();
 void push(int item){
-    if (top=MAX-1){
+    if (top==MAX-1){
         printf("Stack full");
     }
     else {
-        top++;
+        top=top+1;
         stack[top]=item;
     }
 }
 int pop(){
-    if(top=-1){
+    if(top==-1){
         printf("Stack empty");
     }
     else{
-        item=stack[top];
-        top-=1;
+        stack[top];
+        top=top-1;
     }
-    return item;
+    return stack[top];
 }
 int peek(){
     item=stack[top];
@@ -33,18 +33,52 @@ int peek(){
 }
 void display(){
     int i;
-    for(i=0;i<=top;i++){
-        printf("%d/t",stack[i]);
-    }
+	
+	if(top==-1)
+	{
+		printf("\nStack is empty!!");
+	}
+	else
+	{
+		printf("\nElement in Stack are:\n");
+		for(i=top;i>=0;--i)
+			printf("%d\t",stack[i]);
+	}
 }
 void main(){
     int choice;
-   void push(int);
+    void push(int);
     int pop();
     int peek();
     void display();
     while(1){
-
+        printf("1.Push \n2.Pop \n3.Peek \n4.Display \n5.exit\n");
+        printf("Enter your choice :");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("\nInsert an element into stack:");
+            scanf("%d",&item);
+            push(item);
+            break;
+        case 2:
+            pop();
+            printf("Deleted element in stack is %d");
+            break;
+        case 3:
+            printf("topmost element:",peek());
+            break;
+        case 4:
+            printf("Elements present in stack are:\n");
+            display();          
+            break;
+        case 5:
+            exit(0);
+        default:
+            printf("Choose correct option");
+            break;
+        }
     }
 
 }
